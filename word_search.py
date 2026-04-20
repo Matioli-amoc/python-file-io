675191ede3f740a8c2af3fcd35f52e5922f05613
Amanda Matioli <amc0369@auburn.edu>
2026-04-19 19:12:45 -0500

    word_search.py

---
+++ word_search.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+# A script to find regular expressions within a txt file
+import re
+
+if __name__ == "__main__":
+    herit_pattern_str = r'\b(inherit\w*|inher\w*)\b'
+    herit_pattern = re.compile(herit_pattern_str, re.IGNORECASE)
+
+print('Opening origin.txt')
+with open('origin.txt', 'r') as in_stream:
+    print('Opening herit_in_origin.txt')
+    with open('herit_in_origin.txt', 'w') as out_stream:
+        for line_index, line in enumerate(in_stream):
+            line = line.strip()
+            word_list = line.split()
+            for word in word_list:
+                if herit_pattern.search(word):
+                    out_stream.write(f'{line_index + 1}\t{word}\n')
+
+print("Done!")
+print('origin.txt is closed?', in_stream.closed)
+print('herit_in_origin.txt is closed?', out_stream.closed)
